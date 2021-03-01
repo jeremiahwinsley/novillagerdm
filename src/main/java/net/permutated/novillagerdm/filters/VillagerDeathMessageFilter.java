15ce6a079f1f3241b1fe6d98b66f4a6c14d164f0
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2021-02-28 22:47:51 -0500

    Fix potential NPE from Message.getFormat

--- src/main/java/net/permutated/novillagerdm/filters/VillagerDeathMessageFilter.java
+++ src/main/java/net/permutated/novillagerdm/filters/VillagerDeathMessageFilter.java
@@ -7,13 +7,15 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.filter.AbstractFilter;
 import org.apache.logging.log4j.message.Message;
 
+import java.util.Objects;
+
 @Plugin(name = "VillagerDeathMessageFilter", category = Node.CATEGORY, elementType = Filter.ELEMENT_TYPE)
 public class VillagerDeathMessageFilter extends AbstractFilter {
 
     @Override
     public Filter.Result filter(LogEvent event) {
         Message message = event.getMessage();
-        if (message.getFormat().equals("Villager {} died, message: '{}'")) {
+        if (Objects.equals(message.getFormat(), "Villager {} died, message: '{}'")) {
             return Result.DENY;
         } else {
             return Result.NEUTRAL;
